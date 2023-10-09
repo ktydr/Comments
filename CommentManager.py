08219a3ea8638262bd5a147d81adc3d1e36751d3
ktydr <katerynadrob@gmail.com>
2023-10-09 20:34:52 +0200

    core project structure

---
+++ CommentManager.py
@@ -0,0 +1,12 @@
+
+
+class CommentManager:
+
+    def __init__(self):
+        pass
+
+    def learn_from_comments(self, comments):
+        pass
+
+    def test_comments(self, comments):
+        pass
---
+++ main.py
@@ -0,0 +1,27 @@
+from CommentManager import CommentManager 
+
+def main():
+
+    manager = CommentManager()
+
+    comments = fetch_training_comments()
+    test_comments = fetch_test_comments()
+
+    manager.learn_from_comments(comments)
+
+    manager.test_comments(test_comments)
+    print('hejfdksl')
+    """
+    while True:
+        manager.input_comment()
+    """
+     
+def fetch_training_comments():
+    return []
+
+def fetch_test_comments ():
+    return []
+
+
+if __name__ == "__main__":
+    main()
