036902bc352a698677ac4905bef22989d6c3dc52
Vovsanka <drobitkovolodya@gmail.com>
2023-10-10 21:46:45 +0200

    parse comment to words (includes numbers)

--- CommentManager.py
+++ CommentManager.py
@@ -1,13 +1,21 @@
 import numpy as np
+import re
+
 
 class CommentManager:
 
     def __init__(self):
         pass
 
+    def learn_from_comments(self, comments):
+        CommentManager.parse_comment("")
+
     @staticmethod 
     def parse_comment(comment: str) -> list[str]:
-        pass
+        comment = "The way 54 is too hard, but We will overwelm it!"
+        words = re.findall(r"\w+", comment.lower())
+        mark_name = words.pop()
+        return words, mark_name
 
     def tokenize_words(self, words: list[str]) -> None:
         pass
